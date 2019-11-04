da54c0e3213c435c69da7eb1285fffafa9390909
ajinoxx <57328840+ajinoxx@users.noreply.github.com>
2019-11-04 15:15:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,35 +4,43 @@ var myLife = 0;
 var userMsg;
 var spaceIndex;
 var num;
-var role;
+var memRank;
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
 client.on('message', message => {
-	if (message.member.hasPermission('DEAFEN_MEMBERS')){
-		rank = message.member.highestRole.name;
-		message.channel.send(rank);
-		if(message.content.startsWith(`%oml`)){
-			myLife++;
-			message.channel.send("There are now " + myLife + " things on my life.")
-		}
-		else if(message.content.startsWith(`%setoml`)){
-			userMsg = message.content;
-			spaceIndex = userMsg.indexOf(" ") + 1;
-			num = userMsg.substring(spaceIndex);
-			if (!(isNaN(num))) {
-				myLife = num;
+	if (message.content.startsWith(`%`)) {
+		memRank = message.member.highestRole.name;
+		if (memRank == "Slightly more intellectual" || memRank == "alexa"){
+			if(message.content.startsWith(`%oml`)){
+				myLife++;
 				message.channel.send("There are now " + myLife + " things on my life.")
 			}
-			else {
-				message.channel.send("Not a valid number!");
+			else if(message.content.startsWith(`%setoml`)){
+				userMsg = message.content;
+				spaceIndex = userMsg.indexOf(" ") + 1;
+				num = userMsg.substring(spaceIndex);
+				if (!(isNaN(num))) {
+					myLife = num;
+					message.channel.send("There are now " + myLife + " things on my life.")
+				}
+				else {
+					message.channel.send("Not a valid number!");
+				}
+			}
+			else if(message.content.startsWith(`%count`)){
+				message.channel.send("There are " + myLife + " things on my life right now.")
 			}
 		}
-		else if(message.content.startsWith(`%count`)){
-			message.channel.send("There are " + myLife + " things on my life right now.")
+		else{
+		message.channel.send("You are too stupid for this command!")
 		}
+	}
+
+	if(message.content.startsWith(`im`) || message.content.startsWith(`Im`) || message.content.startsWith(`IM`) || message.content.startsWith(`I'M`) || message.content.startsWith(`i'm`) || message.content.startsWith(`I'm`)){
+		message.channel.send("Hi " + message.content.substring(message.content.indexOf(" ")) + ". I'm Irvin's mom.");
 	}
 })
 
